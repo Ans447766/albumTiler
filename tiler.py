57fe5a126578cdeea7f0e01f031ea2805895fe15
Ans Ishtiaq <ans447766@gmail.com>
2019-03-11 19:00:48 +0500

    ok

--- tiler.py
+++ tiler.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 from os.path import expanduser
+from pathlib import Path
 def series():
     image_path = raw_input('path to image: ')
     with Image.open(image_path) as img:
@@ -15,11 +16,13 @@ def series():
     x1 = 0
     y1 = 0
     x2 = y2 = a
+    img_name = Path(image_path).stem
+    os.mkdir(str(img_name) + "tiled")
     for x in range(i):
         for y in range(4):
-            saved_location = "results/" + str(name) + '.jpg'
+            # create folder with image name
+            saved_location = img_name + 'tiled/' + str(name) + '.jpg'
             coords = (x1,y1,x1+x2,y1+y2)
-            image_path = 'sample.jpg'
             imgO = Image.open(image_path)
             cropped_image = imgO.crop(coords)
             cropped_image.save(saved_location)
@@ -27,5 +30,5 @@ def series():
             name = name + 1
         y1 = y1 + int(a)
         x1 = 0
-    print('TILLING_COMPLETED -------------- ' + str(name - 1) + ' TILES EXPORTED')
+    print('TILLING_COMPLETED ----------------------------> ' + str(name - 1) + ' TILES EXPORTED')
 series()
